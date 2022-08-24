aa3c8f81cfb73006ea786090f3d2589a73bab31c
11AlexanderB <bezhenar93@internet.ru>
2022-08-24 11:02:44 +0300

    2

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int X = Convert.ToInt32(Console.ReadLine());
+int X1 = X/10;
+int X2 = X1%10;
+Console.WriteLine("Вторая цифра числа: ");
+Console.WriteLine(X2);
---
+++ 2zadanie/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+while(X>999)
+{
+    X = X/10;
+}
+if(X/100==0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    X = X%10;
+Console.WriteLine("Третья цифра: ");
+Console.WriteLine(X);
+}
