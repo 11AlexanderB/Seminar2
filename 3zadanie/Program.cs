6cfbc1a5028475e2fc3ee33e182895f29a0106bc
11AlexanderB <bezhenar93@internet.ru>
2022-08-25 11:33:52 +0300

    5

--- 3zadanie/Program.cs
+++ 3zadanie/Program.cs
@@ -1,14 +1,8 @@
 ﻿Console.WriteLine("Введите число от 1 до 7: ");
 int X = Convert.ToInt32(Console.ReadLine());
- if(X<6)
- {
-    Console.WriteLine("Рабочий день ");
- }
-else if(X>7)
-{
-    Console.WriteLine("Неверное число ");
-}
-else
-{
-    Console.WriteLine("Выходной день ");
-}
+
+ if(X<6) Console.WriteLine("Рабочий день ");
+
+else if(X>7)  Console.WriteLine("Неверное число ");
+
+else   Console.WriteLine("Выходной день ");
